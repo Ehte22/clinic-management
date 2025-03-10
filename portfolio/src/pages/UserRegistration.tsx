0fe0a7ec21a05d660cc400194727813e223cfcb5
Ehte22 <syedehteshamali22@gmail.com>
2025-03-10 13:39:01 +0530

    domain updated

--- portfolio/src/pages/UserRegistration.tsx
+++ portfolio/src/pages/UserRegistration.tsx
@@ -1,5 +1,4 @@
 import { CheckCircleIcon } from "@heroicons/react/24/outline"
-import { useNavigate } from "react-router-dom"
 import { z } from "zod"
 import { FieldConfig } from "../models/fieldConfig.interface"
 import { useEffect, useState } from "react"
@@ -66,9 +65,6 @@ const defaultValues = {
 }
 
 const UserRegistration = () => {
-
-    // hooks
-    const navigate = useNavigate()
 
     // States
     const [OTP, setOTP] = useState("")
